ec21fb20db962c6fca239811edabba6b67c4ba5e
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-08-13 10:05:17 +0530

    Struct and Pattern Matching

---
+++ struct.rs
@@ -0,0 +1,16 @@
+
+struct User{
+    name: String,
+    age: u32,
+    active: bool
+}
+
+fn main(){
+    let name: String = String::from("Alice");
+    let user: User = User{
+        name: name,
+        age: 30,
+        active: true
+    };
+    print!("{} is {} years old.", user.name, user.age)
+}
